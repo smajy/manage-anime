890fe5ce6de8c5c539f57adf15d47487140dbd0f
smajy <smajy@DESKTOP-VL4DL3T>
2021-01-03 18:27:50 +0330

    #14 #15 add test

--- test/persons/TestPerson.java
+++ test/persons/TestPerson.java
@@ -1,6 +1,8 @@
 package persons;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -17,5 +19,32 @@ public class TestPerson {
 		assertNotNull(user);
 		
 	}
+
+	@Test
+	public void TestLoging() {
+		Admin admin = new Admin("admin", "admin");
+		assertTrue(admin.login(admin));
+		
+		Admin adminFake = new Admin("fake", "fake");
+		assertFalse(adminFake.login(adminFake));
+		
+		User user = new User("user", "user");
+		assertTrue(user.login(user));
+		
+		User userFake = new User("fake", "fake");
+		assertFalse(userFake.login(userFake));
+		
+		
+	}
+
+
+	@Test
+	public void TestRegisterd() {
+		Admin admin = new Admin("admin", "admin");
+		admin.register(admin);
+		
+		User user = new User("user", "user");
+		user.register(user);
 	
+	}
 }
