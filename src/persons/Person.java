746dfff13e8c21dfaef3e157db95ff058b15c899
smajy <smajy@DESKTOP-VL4DL3T>
2021-01-03 17:12:46 +0330

    #13 #16 register and login and save data

--- src/persons/Person.java
+++ src/persons/Person.java
@@ -33,6 +33,80 @@ public class Person {
 		this.password = password;
 	}
 
+	/**
+	 * this method is read file and save admin and user data.
+	 * 
+	 * @param person
+	 * @return boolean. true, mean this person is registered.
+	 */
+	public boolean register(Person person) {
 
+		try {
+			File file = new File("../requireData/admin-user");
+
+			Scanner scanner = new Scanner(file);
+			FileWriter writer = new FileWriter(file);
+
+			String adminData = scanner.nextLine();
+			String userData = scanner.nextLine();
+
+			if (person instanceof Admin) {
+				adminData = "admin = " + person.getName() + "#" + person.getPassword();
+
+			} else if (person instanceof User) {
+				userData = "User = " + person.getName() + "#" + person.getPassword();
+			}
+
+			writer.write(adminData);
+			writer.write("/n");
+			writer.write(userData);
+
+			scanner.close();
+			writer.close();
+			return true;
+		} catch (IOException e1) {
+			System.out.println("file data admin and user does not open");
+		}
+		return false;
+
+	}
+
+	/**
+	 * this method is read file and control for equal admin and user data.
+	 * 
+	 * @param person
+	 * @return boolean. true, mean this person is logined.
+	 */
+	public boolean login(Person person) {
+
+		try {
+			boolean login = false;
+			File file = new File("../requireData/admin-user");
+			Scanner scanner = new Scanner(file);
+
+			String adminData = scanner.nextLine();
+			String[] admin = adminData.substring(adminData.indexOf("=")).trim().split("#");
+
+			String userData = scanner.nextLine();
+			String[] user = userData.substring(userData.indexOf("=")).trim().split("#");
+
+			if (person instanceof Admin) {
+				if (person.getName().equalsIgnoreCase(admin[0]) && person.getPassword().equalsIgnoreCase(admin[1]))
+					login = true;
+			} else if (person instanceof User) {
+				if (person.getName().equalsIgnoreCase(user[0]) && person.getPassword().equalsIgnoreCase(user[1]))
+					login = true;
+			}
+
+			scanner.close();
+			return login;
+
+		} catch (FileNotFoundException e) {
+			System.out.println("file data admin and user does not open");
+		}
+
+		return false;
+
+	}
 
 }
