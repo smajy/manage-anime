732ab6ca835a30f8cf1fbf39c25ae0add78bc709
smajy <smajy@DESKTOP-VL4DL3T>
2021-01-04 15:43:00 +0330

    #19 #21 add method update AML to Admin

--- src/persons/Admin.java
+++ src/persons/Admin.java
@@ -13,6 +13,11 @@ public class Admin extends Person{
 		super(name, password);
 	}
 
+	/**
+	 * this method add AML to List in Control class.
+	 * @param aml
+	 * @return
+	 */
 	public boolean addAML(AML aml) {
 		List<AML> list = Control.getLists();
 		
@@ -20,5 +25,27 @@ public class Admin extends Person{
 			return false;
 		list.add(aml);
 		return true;
+	}
+	
+	/**
+	 * this method search aml.
+	 * if serach is correct so update aml.
+	 * else return false
+	 * @param amlOld
+	 * @param amlNew
+	 * @return
+	 */
+	public boolean updateAML(AML amlOld, AML amlNew) {
+		List<AML> list = Control.getLists();
+		
+		if(list == null)
+			return false;
+		int in = list.indexOf(amlOld);
+		if(in != -1) {
+			list.remove(in);
+			list.add(amlNew);
+			return true;
+		}
+		return false;
 	}
 }
