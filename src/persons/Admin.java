8119b0c3246eb83c1dec4fc63a9e678a3f16dff9
smajy <smajy@DESKTOP-VL4DL3T>
2021-01-04 15:36:28 +0330

    #18 #20 add method add to admin

--- src/persons/Admin.java
+++ src/persons/Admin.java
@@ -1,9 +1,24 @@
 package persons;
 
+
+import java.util.List;
+
+import AML.AML;
+import control.Control;
+
+
 public class Admin extends Person{
 
 	public Admin(String name, String password) {
 		super(name, password);
 	}
 
+	public boolean addAML(AML aml) {
+		List<AML> list = Control.getLists();
+		
+		if(list == null)
+			return false;
+		list.add(aml);
+		return true;
+	}
 }
