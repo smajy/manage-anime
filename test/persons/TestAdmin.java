90d32c96f8aba3f7e0f23300e1c3ebdf10ab6ee0
smajy <smajy@DESKTOP-VL4DL3T>
2021-01-04 16:17:49 +0330

    #27 add test update anime

--- test/persons/TestAdmin.java
+++ test/persons/TestAdmin.java
@@ -1,5 +1,6 @@
 package persons;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
@@ -39,4 +40,34 @@ public class TestAdmin {
 		assertEquals(amlAdd, list.get(in));
 	}
 
+	@Test
+	public void testUpdateAML() {
+		
+		Admin admin = new Admin("name", "password");
+		
+		// aml add to list
+		Anime anime = new Anime("OOO", "productionYear", "nameManufacturer", true, 5, 5, Genres.ACTION, null, "");
+		Manga manga = new Manga("OOO", "productionYear", "nameManufacturer", true, 5, 5, Genres.ACTION, null, "");
+		LightNovel lightNovel = new LightNovel("OOO", "productionYear", "nameManufacturer", true, 5, 5, Genres.ACTION, null, "");
+		AML amlOld = new AML(anime, manga, lightNovel);
+		admin.addAML(amlOld);
+
+		// aml update in list
+		anime = new Anime("OOO", "pY", "nameManufacturer", true, 5, 5, Genres.ACTION, null, "");
+		manga = new Manga("OOO", "productionYear", "nameManufacturer", true, 5, 5, Genres.ACTION, null, "");
+		lightNovel = new LightNovel("OOO", "productionYear", "nameManufacturer", true, 5, 5, Genres.ACTION, null, "");
+		AML amlNew = new AML(anime, manga, lightNovel);
+		
+		List<AML> list = Control.getLists();
+		int size = list.size();
+		admin.updateAML(amlOld, amlNew);
+		
+		assertEquals(size, list.size());
+		
+		int in = list.indexOf(amlNew);
+		AML aml = list.get(in);
+		assertNotNull(aml);
+		assertEquals(aml.getAnime().getNameManufacturer(), amlNew.getAnime().getNameManufacturer());
+		
+	}
 }
