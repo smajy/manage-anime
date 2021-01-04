0b5b5b0920ed633ad1d23fe821635cee557a9c5c
smajy <smajy@DESKTOP-VL4DL3T>
2021-01-04 15:29:36 +0330

    #23 create Control and List AMLs

---
+++ src/Control.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import AML.AML;
+
+public class Control {
+
+	private static List<AML> lists;
+	
+	// TODO Load list AML in ???
+	static {
+		lists = new ArrayList<>();
+	}
+	
+	public static List<AML> getLists() {
+		return lists;
+	}
+}
