e34d4e0374a5fca6eb86d0f07325bdae6b1ad510
smajy <smajy@DESKTOP-VL4DL3T>
2021-01-10 21:03:58 +0330

    #42 success

--- test/persons/TestUser.java
+++ test/persons/TestUser.java
@@ -1,14 +1,12 @@
 package persons;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import AML.AML;
@@ -39,6 +37,24 @@ public class TestUser {
 		assertTrue(aml2.getAnime().isEnd());
 		assertEquals("this is good", aml2.getAnime().getDescription());
 		assertEquals(8, aml2.getAnime().getMyScore());
+	}
+	
+	@Test
+	public void testdeleteAML() {
+		User user = new User("name", "password");
+	
+		Anime anime = new Anime("1", "12", "AS", true, 50, 50, Genres.ACTION, null, "");
+		Manga manga = new Manga("1", "12", "AS", true, 50, 50, Genres.ACTION, null, "");
+		LightNovel lightNovel = new LightNovel("1", "12", "AS", true, 50, 50, Genres.ACTION, null, "");
+		AML aml = new AML(anime, manga, lightNovel);
+		List<AML> list = Control.getLists();
+		list.add(aml);
+		
+		assertFalse(user.deleteAML(null, "this is bad"));
+		
+		assertTrue(user.deleteAML(aml, "this is bad"));
+		assertTrue(list.isEmpty());
+		assertEquals(Control.getDeletedAMLs().get(0), aml);
 	}
 
 }
